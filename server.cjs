778572bd907fe3959164dcf330204ea97aeadd45
Manish Gupta <manishgupta724872@gmail.com>
2025-01-25 22:50:06 +0530

    service.cjs update

--- server.cjs
+++ server.cjs
@@ -16,6 +16,6 @@ app.use('/*', (req, res) => {
 });
 
 // start express server on port 5000
-app.listen(process.env.PORT || 4000, () => {
+app.listen(process.env.PORT || 80, () => {
   console.log('server started');
 });
